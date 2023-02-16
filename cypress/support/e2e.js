38ba8308da1bdca24efc6776a178867562c2f491
Walmyr <wlsf82@gmail.com>
2023-02-16 03:45:33 +0100

    Update e2e support main file with imports at the top
    This is a convention and it helps on readability.

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,22 +1,16 @@
-
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-  
-  if (err.message.includes('angular is not defined')) {
-    return false
-  }
-
-  if (err.message.includes('Cannot read properties of undefined')) {
-    return false
-  }
-
- if (err.message.includes("Unexpected token '<'")) {
-    return false
-  }
-  
-})
-
 import 'cypress-plugin-api'
+import 'cypress-plugin-xhr-toggle'
 import './gui_commands'
 import './api_commands'
 
+Cypress.on('uncaught:exception', err => {
+  if (err.message.includes('angular is not defined')) {
+    return false
+  }
+  if (err.message.includes('Cannot read properties of undefined')) {
+    return false
+  }
+ if (err.message.includes("Unexpected token '<'")) {
+    return false
+  }
+})
