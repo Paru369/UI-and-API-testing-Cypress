8a500c96524044949c91593be99d5784098b9d0e
Walmyr <wlsf82@gmail.com>
2023-02-16 03:37:20 +0100

    Refactor API tests
    - To make them independent of each other
    - To make them use the updated fakeContact module (no more need
    for an instance of a class).
    - To destructure request's response's properties (to avoid object.)
    - And finally, to simplify status code assertions.

--- cypress/e2e/api/CRUD_contacts.cy.js
+++ cypress/e2e/api/CRUD_contacts.cy.js
@@ -1,66 +1,46 @@
-import { ContactDataFaker } from "../helpers/data/contact/contactFaker"
+import { fakeContact } from '../../support/fakeContact'
 
-const Data =  new ContactDataFaker
-const contacData = Data.contactData()
+describe('CRUD Contacts', () => {
+  it('Create a contact', () => {
+    cy.api_createContact(fakeContact)
+      .then(({ status, body }) => {
+        expect(status).to.equal(200)
+        expect(body.value[0].Name).to.equal(fakeContact.name)
+        expect(body.value[0].Email).to.equal(fakeContact.email)
+      })
+  })
 
+  context('Pre-condition: Create a contact', () => {
+    beforeEach(() => {
+      cy.api_createContact(fakeContact)
+        .as('contact')
+    })
 
-describe('Create, Read, Update and Delete Contacts', () => {
+    it('Read a contact', () => {
+      cy.api_readAllContacts()
+        .its('status')
+        .should('be.equal', 200)
+    })
 
+    it('Update a contact', () => {
+      cy.api_updateContact(fakeContact)
+        .its('status')
+        .should('be.equal', 200)
+    })
 
-
-  Cypress._.times(1, () => {
-    it('Create a contact', () => {
-
-      cy.api_createContact(contacData)
-        .then(response => {
-          cy.log(response.body.value[0].Id)
-          expect(response.status).to.equal(200)
-          expect(response.body.value[0].Name).to.equal(contacData.name)
-          expect(response.body.value[0].Email).to.equal(contacData.email)
+    it('Delete a contact', () => {
+      cy.get('@contact').its('body.value[0].Id')
+        .then(contactId => {
+          cy.api_deleteContact(contactId)
+            .its('status')
+            .should('be.equal', 200)
         })
-
     })
 
-  })
-
-  it('Read and contact all contacts in cypress log', () => {
-
-    cy.api_readAllContacts()
-      .then(response => {
-        expect(response.status).to.equal(200)
-        cy.log(`Total of contacts is ${(response.body.value.length)},
-        the first is "${(response.body.value[0].Name)}" and the last
-        is "${(response.body.value[response.body.value.length - 1].Name)}"`)
-
-      })
-  })
-
-  it('Update a contact', () => {
-
-    const updatedContact = Data.contactData()
-
-    cy.api_updateContact(updatedContact)
-      .then(response => {
-        expect(response.status).to.equal(200)
-        cy.log(response.body.value[0].Name, response.body.value[0].Email,)
-
-      })
-  })
-
-
-  it('Delete one contact', () => {
-
-    cy.api_deleteOneContact().then(response => {
-      expect(response.status).to.equal(200)
+    it('Delete all contacts', () => {
+      cy.api_deleteAllContacts()
+        .its('status')
+        .should('be.equal', 200)
     })
   })
-
-  it('Delete all contacts', () => {
-
-    cy.api_deleteAllContacts().then(response => {
-      expect(response.status).to.equal(200)
-    })
-  })
-
-
 })
