025dfdd5c30c1c0dea6c607563858ad4b04b70e1
Walmyr <wlsf82@gmail.com>
2023-02-16 03:18:12 +0100

    Update login test description

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,8 +1,7 @@
 describe('Login', () => {
-  it('Logar e confirmar login', () => {
+  it('ao fazer o login o avatar do usuário é exibido', () => {
     cy.login()
     cy.wait(1000)
     cy.contains('Paulo Victor').should('be.visible')
   })
 })
-
