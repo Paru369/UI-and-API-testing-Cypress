37fde4a13d04d87e116fda0d0c3e723394313f78
Walmyr <wlsf82@gmail.com>
2023-02-16 03:15:55 +0100

    Delete files that were not refactored

--- cypress/e2e/helpers/data/lead/leadsFaker.js
+++
@@ -1,8 +0,0 @@
-import { faker } from '@faker-js/faker';
-export const leadDataFaker = {
-    companyName: faker.commerce.productAdjective(),
-    personName: faker.name.firstName(),
-    origin: faker.internet.domainName()
-    
-    }
-      
