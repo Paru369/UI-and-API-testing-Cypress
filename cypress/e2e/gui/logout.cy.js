d918facfb46d4344df51b39ae94986a1873aab2a
Walmyr <wlsf82@gmail.com>
2023-02-16 03:22:47 +0100

    Simplify logout test
    By not using custom cmd for such simple actions.

--- cypress/e2e/gui/logout.cy.js
+++ cypress/e2e/gui/logout.cy.js
@@ -1,8 +1,9 @@
 describe('Logout', () => {
   beforeEach(() => cy.login())
 
-  it('Sucesso ao deslogar', () => {
-    cy.logout()
+  it('redireciona à página de login ao fazer o logout', () => {
+    cy.get('#leftRenderImage', { timeout: 10000 }).click()
+    cy.contains('Sair').click()
 
     cy.url().should('contain', '/login')
   })
