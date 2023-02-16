327926cb022e8142798981f07e6b27f2fcc7133e
Walmyr <wlsf82@gmail.com>
2023-02-16 03:32:47 +0100

    Improve other gui custom cmds
    - By improving element's selections
    - And by destructuring object's properties to avoid object. repea-
    tedly.
    
    Finally, this commit also remove custom commands that are not in
    use anymore, since non-refactored tests were deleted.

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -19,58 +19,26 @@ Cypress.Commands.add('gui_createCustomer', ({ name, phone, email }) => {
   cy.contains('button', 'Salvar').click()
 })
 
-Cypress.Commands.add('gui_readCustomer', name => {
+
+Cypress.Commands.add('gui_readCustomer', ({ name }) => {
   cy.contains('Pesquisar').type(name)
+  cy.contains('.search-panel.open-panel span', name)
+    .should('be.visible')
+    .click()
+})
+
+Cypress.Commands.add('gui_updateCustomer', (newName) => {
+  cy.contains('a', 'Opções').click()
+  cy.contains('a', 'Editar cliente').click()
+  cy.get('input[name="contact_name"]')
+    .clear()
+    .type(newName)
+  cy.contains('button', 'Salvar').click()
 
 })
 
-Cypress.Commands.add('gui_updateCustomer', (actualName, newName) => {
-  cy.contains('Pesquisar').type(actualName)
-  cy.contains(actualName).click()
-  cy.contains('Opções').click()
-  cy.contains('Editar cliente').click()
-  cy.get('input[name="contact_name"]').clear().type(newName)
-  cy.get('button[class="button button-action pull-right"]').click()
-
-})
-
-Cypress.Commands.add('gui_deleteCustomer', name => {
-  cy.contains('Pesquisar').type(name)
-  cy.contains(name).click()
-  cy.contains('Opções').click()
-  cy.contains('Excluir cliente').click()
-  cy.contains('Confirmar').click()
-})
-
-Cypress.Commands.add('gui_createDeal', dealData => {
-  cy.contains('Negócios').click()
-  cy.get('a[class="button button-action pull-right nowrap"]').click()
-  cy.get('input[name="deal_title"]').type(dealData.title)
-  cy.get('input[name="deal_amount"]').type(dealData.amount)
-  cy.get('button[class="button button-action pull-right"]').click()
-  cy.contains('Negócios').click()
-})
-
-
-Cypress.Commands.add('gui_readDeal', name => {
-  cy.contains('Pesquisar').type(name)
-})
-
-Cypress.Commands.add('gui_updateDeal', (actualTitle, newTitle) => {
-  cy.contains('Negócios').click()
-  cy.contains(actualTitle).click()
-
-  cy.contains('Opções').click()
-  cy.contains('Editar negócio').click()
-  cy.get('input[name="deal_title"]').clear().type(newTitle)
-  cy.get('button[class="button button-action pull-right"]').click()
-})
-
-Cypress.Commands.add('gui_deleteDeal', title => {
-  cy.wait(1000)
-  cy.contains('Negócios').click()
-  cy.contains(title).click()
-  cy.contains('Opções').click()
-  cy.contains('Excluir negócio').click()
-  cy.contains('Confirmar').click()
+Cypress.Commands.add('gui_deleteCustomer', () => {
+  cy.contains('a', 'Opções').click()
+  cy.contains('a', 'Excluir cliente').click()
+  cy.contains('a', 'Confirmar').click()
 })
